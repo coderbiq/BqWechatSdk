c90a37b2a2f3c7ef7f16614f9417a311b3cc0b9b
elvis bi <elvis@dwenzi.com>
2013-05-24 16:25:49 +0800

    实现接收到微信推送的文本消息后返回音乐消息的测试

---
+++ test/BqWechatSdkTest/PushTextReturnTest/MusicTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace BqWechatSdkTest\PushTextReturnTest;
+
+use BqWechatSdk\Message;
+use BqWechatSdk\Event\Result;
+
+class MusicTest extends PushTextReturnTestCase
+{
+    public function testPushTextReturnMusion()
+    {
+        $output = $this->getOutput();
+
+        $this->assertInstanceOf('SimpleXMLElement', $output);
+        $this->assertEquals('fromUser', $output->FromUserName);
+        $this->assertEquals('toUser', $output->ToUserName);
+        $this->assertEquals('123456789', $output->CreateTime);
+        $this->assertEquals('0', $output->FuncFlag);
+        $this->assertEquals('music', $output->MsgType);
+        $this->assertEquals('url', $output->MusicUrl);
+        $this->assertEquals('hqurl', $output->HQMusicUrl);
+    }
+
+    public function onPush($event)
+    {
+        $message = new Message();
+        $message
+            ->setType(Message::TYPE_MUSIC)
+            ->setFromUserName('fromUser')
+            ->setToUserName('toUser')
+            ->setCreateTime(123456789)
+            ->setFuncFlag(0)
+            ->setMusicUrl('url')
+            ->setHQMusicUrl('hqurl');
+        $result = new Result();
+        $result->setMessage($message);
+        return $result;
+    }
+}
