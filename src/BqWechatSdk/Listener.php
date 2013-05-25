4e8961b079d1b4f25dfc2552ac2c049737adb058
elvis bi <elvis@dwenzi.com>
2013-05-25 18:35:14 +0800

    添加代码注释

--- src/BqWechatSdk/Listener.php
+++ src/BqWechatSdk/Listener.php
@@ -1,10 +1,33 @@
 <?php
+/**
+ * BqWechatSdk (https://github.com/elvis-bi/BqWechatSdk)
+ *
+ * @link https://github.com/elvis-bi/BqWechatSdk for this canonical source repository
+ * @copyright elvis bi (elvis@dwenzi.com)
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+
 namespace BqWechatSdk;
 
 use Zend\EventManager\EventManager;
 use BqWechatSdk\Event\Result as EventResult;
 use BqWechatSdk\Event\Push as PushEvent;
 
+/**
+ * 类Listener监听来自微信的网址接入验证和消息推送
+ *
+ * 使用的时候先要向Listener设置一个EventManager对象，当收到请求时Listener将通过
+ * EventManager进行事件的广播。
+ *
+ * <code>
+ * $eventManager = new EventManager();
+ * # 绑定监听
+ * $eventManager->attach({事件名},{回调函数});
+ *
+ * $listener = new Listener();
+ * $listener->setEventManager($eventManager)->run();
+ * </code>
+ */
 class Listener
 {
     protected $eventManager;
